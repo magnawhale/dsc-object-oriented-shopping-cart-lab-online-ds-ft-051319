b846d660ae28e05201c208366dcf45a9709e6c60
Matthew Parker <mattheweparker@gmail.com>
2019-06-04 15:43:55 -0500

    working on last line

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,52 @@
 class ShoppingCart:
-    # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+
+    def __init__(self, total=0, employee_discount=None, items=[]):
+        self.total = total
+        self.employee_discount = employee_discount
+        self.items = items
+        
     def add_item(self, name, price, quantity=1):
-       pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.items.append([name, price, quantity])
+        basket_total = []
+        for i in self.items:
+            basket_total.append(i[1]*i[2])
+        self.total = sum(basket_total)
+        return self.total
+
     def mean_item_price(self):
-       pass
+        import numpy as np
+        mean_list = []
+        for e in self.items:
+            for i in range(e[2]):
+                mean_list.append(e[1])
+        return np.mean(mean_list)
 
     def median_item_price(self):
-        pass
+        import numpy as np
+        median_list = []
+        for e in self.items:
+            for i in range(e[2]):
+                median_list.append(e[1])
+        return np.median(median_list)
 
     def apply_discount(self):
-       pass
+        if self.employee_discount != None:
+            self.total = self.total * ((100 - self.employee_discount)/100) 
+            return self.total
+        else:
+            return "Sorry, there is no discount to apply to your cart :("
 
     def void_last_item(self):
-        pass
+        if self.items == []:
+            return "There are no items in your cart!"
+        else:
+            del self.items[-1]
+            basket_total = []
+            for i in self.items:
+                basket_total.append(i[1]*i[2])
+            self.total = sum(basket_total)
+            return self.total
+            
